261f5cc3e41a7c7775c11d2f0499da2ef981bc7a
Kachii <Kacheokcheong@gmail.com>
2020-12-01 17:10:38 +0000

    initial commit

---
+++ src/DummyData.js
@@ -0,0 +1,14 @@
+const DummyData = {
+    "tv_results": [
+        {
+            "title": "Game of Thrones",
+            "imdb_id": "tt0944947",
+            "release_date": "2011-04-17"
+        }
+    ],
+    "search_results": 1,
+    "status": "OK",
+    "status_message": "Query was successful"
+}
+
+export default DummyData
